d43bde25b8ebfee76f3464716588273b6a8f5a06
taijusanagi <taijusanagi@gmail.com>
2022-12-12 20:37:34 +0900

    feat: done

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 import { Layout } from "@/components/Layout";
 import { Modal } from "@/components/Modal";
+import { Step, useStep } from "@/components/Step";
 import { Unit } from "@/components/Unit";
 import { useErrorHandler } from "@/hooks/useErrorHandler";
 
@@ -28,26 +29,50 @@ const HomePage: NextPage = () => {
   const [creators, setCreators] = useState<Creator[]>([]);
   const [metadata, setMetadata] = useState<Metadata>();
   const [share, setShare] = useState(20);
-  const [amount, setAmount] = useState(100);
+  const [amount, setAmount] = useState(10);
   const [userAddress, setUserAddress] = useState("");
-  const [deployedAddress, setDeployedAddress] = useState("");
 
+  const [modalMode, setModalMode] = useState<"preview" | "step">("preview");
   const modalDisclosure = useDisclosure();
 
   const [isLoadingPreview, setIsLoadingPreview] = useState(false);
-  const [isProcessingTx, setIsProcessingTx] = useState(false);
 
   const sdk = useSDK();
+
+  const steps = [
+    {
+      title: "Create collection",
+      description: "Creating fundible copy collections",
+    },
+    {
+      title: "Mint NFT",
+      description: "Mint NFT in created collection",
+    },
+    {
+      title: "Mint additional supply",
+      description: "Mint additional supply for minted NFT",
+    },
+    {
+      title: "Check in thirdweb dashboard",
+      description: "Minted NFT can be checked in thirdweb dashboard",
+    },
+  ];
+
+  const [currentStep, isTxProcessing, { setStep, setIsProcessing }] = useStep({
+    maxStep: steps.length,
+    initialStep: 0,
+  });
 
   const { handle } = useErrorHandler();
 
   const clear = () => {
     modalDisclosure.onClose();
     setIsLoadingPreview(false);
-    setIsProcessingTx(false);
+    setIsProcessing(false);
     setCreators([]);
     setMetadata(undefined);
-    setDeployedAddress("");
+    setModalMode("preview");
+    setStep(0);
   };
 
   return (
@@ -112,6 +137,7 @@ const HomePage: NextPage = () => {
                 modalDisclosure.onOpen();
               } catch (e) {
                 handle(e);
+                clear();
               } finally {
                 setIsLoadingPreview(false);
               }
@@ -121,76 +147,110 @@ const HomePage: NextPage = () => {
           </Button>
           <Modal header="Preview" isOpen={modalDisclosure.isOpen} onClose={clear}>
             {sdk && metadata && creators.length > 0 && (
-              <Stack spacing="4">
-                <Stack>
-                  <Stack spacing="1">
-                    <Text fontSize="sm" fontWeight={"bold"} color={configFileJson.style.color.black.text.secondary}>
-                      Name
-                    </Text>
-                    <Text fontSize="sm" color={configFileJson.style.color.black.text.secondary}>
-                      {metadata.name}
-                    </Text>
+              <>
+                {modalMode === "preview" && (
+                  <Stack spacing="4">
+                    <Stack>
+                      <Stack spacing="1">
+                        <Text fontSize="sm" fontWeight={"bold"} color={configFileJson.style.color.black.text.secondary}>
+                          Name
+                        </Text>
+                        <Text fontSize="sm" color={configFileJson.style.color.black.text.secondary}>
+                          {metadata.name}
+                        </Text>
+                      </Stack>
+                      <Stack spacing="1">
+                        <Text fontSize="sm" fontWeight={"bold"} color={configFileJson.style.color.black.text.secondary}>
+                          Image
+                        </Text>
+                        <HStack justify={"center"}>
+                          <Image src={metadata.image} alt={"image"} w="40" />
+                        </HStack>
+                      </Stack>
+                    </Stack>
+                    <Stack>
+                      <Text fontSize="sm" fontWeight={"bold"} color={configFileJson.style.color.black.text.secondary}>
+                        Royalty Share
+                      </Text>
+                      <Stack spacing="1">
+                        <Text fontSize="x-small">Share: {creators[0].share - share} %</Text>
+                        <Text fontSize="x-small">Address: {creators[0].address}</Text>
+                      </Stack>
+                      <Stack spacing="1">
+                        <Text fontSize="x-small">Share: {share} %</Text>
+                        <Text fontSize="x-small">Address: {userAddress}</Text>
+                      </Stack>
+                    </Stack>
+                    <Stack spacing="1">
+                      <Text fontSize="sm" fontWeight={"bold"} color={configFileJson.style.color.black.text.secondary}>
+                        Amount
+                      </Text>
+                      <Text fontSize="sm" color={configFileJson.style.color.black.text.secondary}>
+                        {amount}
+                      </Text>
+                    </Stack>
+                    <Button
+                      w="full"
+                      onClick={async () => {
+                        try {
+                          setModalMode("step");
+                          setIsProcessing(true);
+                          setStep(0);
+                          setIsProcessing(true);
+                          const createdCollection = await sdk.deployer.createNftCollection({
+                            ...metadata,
+                            name: `Fungible Copy of ${metadata.name}`,
+                            symbol: `FC`,
+                            creators: [
+                              { address: creators[0].address, share: creators[0].share - share },
+                              { address: userAddress, share },
+                            ],
+                          });
+                          setIsProcessing(false);
+                          console.log("createdCollection", createdCollection);
+
+                          const program = await sdk.getProgram(createdCollection, "nft-collection");
+                          setStep(1);
+                          setIsProcessing(true);
+                          const nftAddress = await program.mint({
+                            ...metadata,
+                            name: `Fungible Copy of ${metadata.name}`,
+                          });
+                          setIsProcessing(false);
+                          console.log("nftAddress", nftAddress);
+                          setStep(2);
+                          setIsProcessing(true);
+                          await program.mintAdditionalSupply(nftAddress, amount);
+                          setIsProcessing(false);
+                          console.log("done");
+                        } catch (e) {
+                          handle(e);
+                          clear();
+                        }
+                      }}
+                    >
+                      Create Fungible Copy
+                    </Button>
                   </Stack>
-                  <Stack spacing="1">
-                    <Text fontSize="sm" fontWeight={"bold"} color={configFileJson.style.color.black.text.secondary}>
-                      Image
-                    </Text>
-                    <HStack justify={"center"}>
-                      <Image src={metadata.image} alt={"image"} w="40" />
-                    </HStack>
-                  </Stack>
-                </Stack>
-                <Stack>
-                  <Text fontSize="sm" fontWeight={"bold"} color={configFileJson.style.color.black.text.secondary}>
-                    Royalty Share
-                  </Text>
-                  <Stack spacing="1">
-                    <Text fontSize="x-small">Share: {creators[0].share - share} %</Text>
-                    <Text fontSize="x-small">Address: {creators[0].address}</Text>
-                  </Stack>
-                  <Stack spacing="1">
-                    <Text fontSize="x-small">Share: {share} %</Text>
-                    <Text fontSize="x-small">Address: {userAddress}</Text>
-                  </Stack>
-                </Stack>
-                <Stack spacing="1">
-                  <Text fontSize="sm" fontWeight={"bold"} color={configFileJson.style.color.black.text.secondary}>
-                    Amount
-                  </Text>
-                  <Text fontSize="sm" color={configFileJson.style.color.black.text.secondary}>
-                    {amount}
-                  </Text>
-                </Stack>
-                <Button
-                  w="full"
-                  isLoading={isProcessingTx}
-                  onClick={async () => {
-                    try {
-                      setIsProcessingTx(true);
-                      const deployedAddress = await sdk.deployer.createNftCollection({
-                        ...metadata,
-                        name: `Fungible Copy of ${metadata.name}`,
-                        symbol: `FC`,
-                        creators: [
-                          { address: creators[0].address, share: creators[0].share - share },
-                          { address: userAddress, share },
-                        ],
-                      });
-                      console.log(deployedAddress);
-                      setDeployedAddress(deployedAddress);
-                      const program = await sdk.getProgram(deployedAddress, "nft-collection");
-                      const nftAddress = await program.mint({ ...metadata, name: `Fungible Copy of ${metadata.name}` });
-                      await program.mintAdditionalSupply(nftAddress, 10);
-                    } catch (e) {
-                      handle(e);
-                    } finally {
-                      setIsProcessingTx(false);
-                    }
-                  }}
-                >
-                  Create Fungible Copy
-                </Button>
-              </Stack>
+                )}
+                {modalMode === "step" && (
+                  <>
+                    {steps.map((step, id) => (
+                      <Step
+                        key={id}
+                        cursor="pointer"
+                        onClick={() => setStep(id)}
+                        title={step.title}
+                        description={step.description}
+                        isActive={currentStep === id}
+                        isCompleted={currentStep > id}
+                        isProcessing={isTxProcessing}
+                        isLastStep={steps.length === id + 1}
+                      />
+                    ))}
+                  </>
+                )}
+              </>
             )}
           </Modal>
         </Stack>
